8c86973da55c8719fb2a85258658538d1ddf8771
Rin-Nina <nina.mouzain@gmail.com>
2024-12-29 18:37:04 +0000

    added the reset btn

--- script.js
+++ script.js
@@ -11,16 +11,31 @@ const currentScore0EL = document.querySelector('#current--0');
 const currentScore1EL = document.querySelector('#current--1');
 const player0EL = document.querySelector('.player--0');
 const player1EL = document.querySelector('.player--1');
-// setting starting condition
 
-score0EL.textContent = '0';
-score1EL.textContent = '0';
-diceEL.classList.add('hidden');
-//
-let currentScore = 0;
-let activePlayer = 0;
-const score = [0, 0];
-let palying = true;
+let currentScore;
+let activePlayer;
+let score;
+let palying;
+//initialisation function
+
+const init = function () {
+  currentScore0EL.textContent = 0;
+  currentScore1EL.textContent = 0;
+  player0EL.classList.add('player--active');
+  player1EL.classList.remove('player--active');
+  player0EL.classList.remove('player--winner');
+  player0EL.classList.remove('player--winner');
+  score0EL.textContent = '0';
+  score1EL.textContent = '0';
+  diceEL.classList.add('hidden');
+
+  currentScore = 0;
+  activePlayer = 0;
+  score = [0, 0];
+  palying = true;
+};
+
+init();
 
 let switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -80,5 +95,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
 // reset the game
-btnNew.addEventListener('click', function () {});
+btnNew.addEventListener('click', init);
